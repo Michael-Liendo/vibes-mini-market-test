2a9569bf4f3bbdbe8b1bd4f7608a0b86e43b5783
Michael Liendo <michael.m.liendo.r@gmail.com>
2025-08-29 14:08:14 -0400

    feat: add dotenv configuration and cors support, update package dependencies

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,12 +1,27 @@
+import cors from "cors";
+import dotenv from "dotenv";
 import express from "express";
 
-const app = express();
-const port = 3000;
+dotenv.config();
 
-app.get("/", (req, res) => {
+const app = express();
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || "0.0.0.0";
+
+app.use(
+	cors({
+		origin: process.env.CORS_ORIGIN?.split(","),
+	}),
+);
+
+app.get("/", (_, res) => {
 	res.send("Hello World!");
 });
 
-app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
+app.listen({ port: Number(port), host }, (err?: Error) => {
+	if (err) {
+		console.error("Error starting server:", err);
+		process.exit(1);
+	}
+	console.log(`Server listening at http://${host}:${port}`);
 });
